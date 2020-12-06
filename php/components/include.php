e81e482ed36051093b6bdf640723c4e0f600cbf2
IQuinteros <i.quinteros@hotmail.com>
2020-12-06 13:53:27 -0300

    Base style added

--- php/components/include.php
+++ php/components/include.php
@@ -9,9 +9,19 @@
 */
 
 /*
-    Add all third parties api's - libraries
+    Add all third parties api's - libraries and styles
 */
 
 ?>
 
 <script src="/js/third-party/jquery.js"></script>
+
+<!-- Fonts -->
+<link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Audiowide&family=Montserrat:wght@300;400;500;600&display=swap" rel="stylesheet">
+
+<!-- Animations -->
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+<!-- Styles -->
+<link rel="stylesheet" href="/css/style.css">
